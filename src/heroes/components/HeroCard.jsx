a24a579f4acd09bdfe020fa64e3ab9ffd7c2a551
Sergio Reinoso <slrs993@gmail.com>
2023-12-11 23:00:07 -0500

    Read args for URL

--- src/heroes/components/HeroCard.jsx
+++ src/heroes/components/HeroCard.jsx
@@ -11,41 +11,41 @@ export const HeroCard = ({
 
     const heroImageUrl = `/assets/heroes/${ id }.jpg`
 
-  return (
-    <div className="col">
-        <div className="card">
+    return (
+        <div className="col">
+            <div className="card">
 
-            <div className="row no-gutters">
-                <div className="col-4">
-                    <img src={heroImageUrl} className="card-img" alt={superhero} />
-                </div>
+                <div className="row no-gutters">
+                    <div className="col-4">
+                        <img src={heroImageUrl} className="card-img" alt={superhero} />
+                    </div>
 
-                <div className="col-8">
-                    <div className="card-body">
-                        <h5 className="card-title">
-                            {superhero}
-                        </h5>
-                        <p className="card-text">
-                            { alter_ego }
-                        </p>
-                        {
-                            ( alter_ego !== characters ) && (<p>{characters}</p>)
-                        }
-                        <p className="card-text">
-                            <small className="text-muted">
-                                { first_appearance }
-                            </small>
-                        </p>
+                    <div className="col-8">
+                        <div className="card-body">
+                            <h5 className="card-title">
+                                {superhero}
+                            </h5>
+                            <p className="card-text">
+                                { alter_ego }
+                            </p>
+                            {
+                                ( alter_ego !== characters ) && (<p>{characters}</p>)
+                            }
+                            <p className="card-text">
+                                <small className="text-muted">
+                                    { first_appearance }
+                                </small>
+                            </p>
 
-                        <Link to={`/hero/${id}`}>
-                            Más info...
-                        </Link>
+                            <Link to={`/hero/${id}`}>
+                                Más info...
+                            </Link>
 
+                        </div>
                     </div>
                 </div>
-            </div>
 
+            </div>
         </div>
-    </div>
-  )
+    )
 }
